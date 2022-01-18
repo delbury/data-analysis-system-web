a9d7d453d309c2082604be1c9cd6bcddd7426697
hujiqing <hujiqing@bytedance.com>
2022-01-18 11:43:37 +0800

    feat: 完善404页面

--- src/pages/error/404.vue
+++ src/pages/error/404.vue
@@ -1,14 +1,25 @@
 <template>
-  <div></div>
+  <div class="page page-404 flex-center-v">
+    <h3>没有权限或者无效路径</h3>
+    <el-button @click="goHome">
+      返回首页
+    </el-button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'PageError404',
   setup() {
-    return {};
+    const router = useRouter();
+    return {
+      goHome: () => {
+        router.push('/');
+      },
+    };
   },
 });
 </script>
